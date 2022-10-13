a0bc2843c6aae0d4209862c172eefe870dca978a
aeiiver <101661283+aeiiver@users.noreply.github.com>
2022-10-14 00:35:06 +0200

    build: set build directory target

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/four-connect/"
+  base: "/connect-four/",
+  build: {
+    assetsDir: ""
+  }
 })
